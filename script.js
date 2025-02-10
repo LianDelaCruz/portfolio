04e1352775ac2a4898d9a46593357cba313b3894
Takikato09 <liandelacruz09@hotmail.com>
2025-02-10 21:43:54 +0100

    refactoring styles

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById("main-button").addEventListener("click", function() {
     window.location.href = "main.html";
 });
+
+
+
+
+/*   document.querySelector(".hamburger").addEventListener("click", function () {
+    document.querySelector(".nav-links").classList.toggle("active");
+  }); */
